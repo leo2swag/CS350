300b71087929ac9745a852b81892b60b8581e12b
leo2swag <leoliush1137@gmail.com>
2018-05-28 19:15:55 -0400

    from mac q2 final2

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -6,6 +6,9 @@
 
 static int volatile ways[4][3] = {{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
 
+static bool requestStop = false;
+static Direction curOrigin;
+static Direction curDestination;
 bool intersection_no_hit(Direction origin, Direction destination);
 void one_intersection_end(Direction origin, Direction destination);
 /* 
@@ -27,6 +30,7 @@ void one_intersection_end(Direction origin, Direction destination);
  */
 static struct lock *intersectionLock;
 static struct cv *intersectionCv;
+static struct cv *fintersectionCv;
 
 
 
@@ -48,6 +52,10 @@ intersection_sync_init(void)
 	if (intersectionCv == NULL) {
 		panic("could not create intersection cv");
 	}
+	fintersectionCv = cv_create("fintersectionCv");
+	if (fintersectionCv == NULL) {
+		panic("could not create fintersection cv");
+	}
 	return;
 }
 
@@ -64,117 +72,119 @@ intersection_sync_cleanup(void)
 {
 	KASSERT(intersectionLock != NULL);
 	KASSERT(intersectionCv != NULL);
+	KASSERT(fintersectionCv != NULL);
 	lock_destroy(intersectionLock);
 	cv_destroy(intersectionCv);
+	cv_destroy(fintersectionCv);
 }
 
 bool
 intersection_no_hit(Direction origin, Direction destination) {
 	if (origin == 0 && destination == 1) {
-		if (ways[1][2] != 0||
-			ways[1][3] != 0||
-			ways[2][3] != 0||
-			ways[2][1] != 0||
-			ways[2][0] != 0||
-			ways[3][0] != 0||
-			ways[3][1]!= 0
+		if (ways[1][2]||
+			ways[1][3]||
+			ways[2][3]||
+			ways[2][1]||
+			ways[2][0]||
+			ways[3][0]||
+			ways[3][1]
 			) {
 				return false;
 			}
 	} else if (origin == 0 && destination == 2) {
-		if (ways[1][2] != 0||
-			ways[1][3] != 0||
-			ways[2][3] != 0||
-			ways[3][0] != 0||
-			ways[3][1] != 0||
-			ways[3][2] != 0
+		if (ways[1][2]||
+			ways[1][3]||
+			ways[2][3]||
+			ways[3][0]||
+			ways[3][1]||
+			ways[3][2]
 			) {
 				return false;
 			}
 	} else if (origin == 0 && destination == 3) {
-		if (ways[2][3] != 0 ||
-			ways[1][3] != 0
+		if (ways[2][3]||
+			ways[1][3]
 			) {
 				return false;
 			}
 	} else if (origin == 1 && destination == 0) {
-		if (ways[2][0] != 0||
-			ways[3][0] != 0
+		if (ways[2][0]||
+			ways[3][0]
 			) {
 				return false;
 			}
 	} else if (origin == 1 && destination == 2) {
-		if (ways[0][1] != 0||
-			ways[0][2] != 0||
-			ways[2][3] != 0||
-			ways[2][0] != 0||
-			ways[3][2] != 0||
-			ways[3][0] != 0||
-			ways[3][1] != 0
+		if (ways[0][1]||
+			ways[0][2]||
+			ways[2][3]||
+			ways[2][0]||
+			ways[3][2]||
+			ways[3][0]||
+			ways[3][1]
 			) {
 				return false;
 			}
 	} else if (origin == 1 && destination == 3) {
-		if (ways[0][1] != 0||
-			ways[0][2] != 0||
-			ways[0][3] != 0||
-			ways[2][3] != 0||
-			ways[2][0] != 0||
-			ways[3][0] != 0
+		if (ways[0][1]||
+			ways[0][2]||
+			ways[0][3]||
+			ways[2][3]||
+			ways[2][0]||
+			ways[3][0]
 			) {
 				return false;
 			}
 	} else if (origin == 2 && destination == 0) {
-		if (ways[0][1] != 0||
-			ways[1][2] != 0||
-			ways[1][3] != 0||
-			ways[1][0] != 0||
-			ways[3][0] != 0||
-			ways[3][1] != 0
+		if (ways[0][1]||
+			ways[1][2]||
+			ways[1][3]||
+			ways[1][0]||
+			ways[3][0]||
+			ways[3][1]
 			) {
 				return false;
 			}
 	} else if (origin == 2 && destination == 1) {
-		if (ways[3][1] != 0||
-			ways[0][1] != 0
+		if (ways[3][1]||
+			ways[0][1]
 			) {
 				return false;
 			} 
 	} else if (origin == 2 && destination == 3) {
-		if (ways[0][1] != 0||
-			ways[0][2] != 0||
-			ways[1][2] != 0||
-			ways[1][3] != 0||
-			ways[1][0] != 0||
-			ways[2][3] != 0||
-			ways[2][0] != 0
+		if (ways[0][1]||
+			ways[0][2]||
+			ways[1][2]||
+			ways[1][3]||
+			ways[1][0]||
+			ways[2][3]||
+			ways[2][0]
 			) {
 				return false;
 			}
 	} else if (origin == 3 && destination == 0) {
-		if (ways[0][1] != 0||
-			ways[0][2] != 0||
-			ways[1][2] != 0||
-			ways[1][3] != 0||
-			ways[1][0] != 0||
-			ways[2][3] != 0||
-			ways[2][0] != 0
+		if (ways[0][1]||
+			ways[0][2]||
+			ways[1][2]||
+			ways[1][3]||
+			ways[1][0]||
+			ways[2][3]||
+			ways[2][0]
 			) {
 				return false;
 			}
 	} else if (origin == 3 && destination == 1) {
-		if (ways[0][1] != 0||
-			ways[0][2] != 0||
-			ways[1][2] != 0||
-			ways[2][3] != 0||
-			ways[2][0] != 0||
-			ways[2][1] != 0
+		if (ways[0][1]||
+			ways[0][2]||
+			ways[1][2]||
+			ways[2][3]||
+			ways[2][0]||
+			ways[2][1]
 			) {
 				return false;
 			}
 	} else if (origin == 3 && destination == 2) {
-		if (ways[0][2] != 0||
-			ways[1][2] != 0
+		if (ways[0][2]||
+			ways[1][2]
 			) {
 				return false;
 			}
@@ -200,13 +210,22 @@ intersection_no_hit(Direction origin, Direction destination) {
 void
 intersection_before_entry(Direction origin, Direction destination) 
 {
+	KASSERT(intersectionCV != NULL);
+  	KASSERT(fintersectionCv != NULL);
 	KASSERT(intersectionLock != NULL);
 	lock_acquire(intersectionLock);
-	while (!intersection_no_hit(origin, destination)) {
-		cv_wait(intersectionCv, intersectionLock);
-		
+	while ((requestStop && origin != curOrigin && destination != curDestination)||!intersection_no_hit(origin, destination)) {
+		//cv_wait(intersectionCv, intersectionLock);
+		if (requestStop == false) {
+      		requestStop = true;
+      		cv_wait(fintersectionCv, intersectionLock);
+    	} else {
+      		cv_wait(intersectionCV, intersectionLock);
+    	}
 	}
-	cv_signal(intersectionCv, intersectionLock);
+	//cv_signal(intersectionCv, intersectionLock);
+	curOrigin = origin;
+ 	curDestination = destination;
 	lock_release(intersectionLock);
 }
 
@@ -226,8 +245,12 @@ void
 intersection_after_exit(Direction origin, Direction destination) 
 {
 	KASSERT(intersectionLock != NULL);
+	KASSERT(intersectionCV != NULL);
+  	KASSERT(fintersectionCv != NULL);
 	lock_acquire(intersectionLock);
 	ways[origin][destination]--;
-	cv_signal(intersectionCv, intersectionLock);
+	requestStop = false;
+	cv_signal(fintersectionCv, intersectionLock);
+	cv_broadcast(intersectionCv, intersectionLock);
 	lock_release(intersectionLock);
 }
