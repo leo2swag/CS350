e9eb56af58ed1422d684e85b246920b9e40cf2e5
leo2swag <leoliush1137@gmail.com>
2018-05-28 19:27:56 -0400

    from mac q2 finalt

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -4,63 +4,73 @@
 #include <synch.h>
 #include <opt-A1.h>
 
-bool allowed(Direction origin, Direction destination);
+static int ns = 0;
+static int ne = 0;
+static int nw = 0;
+static int sn = 0;
+static int sw = 0;
+static int se = 0;
+static int ew = 0;
+static int en = 0;
+static int es = 0;
+static int we = 0;
+static int wn = 0;
+static int ws = 0;
+static bool requestStop = false;
+static Direction curOrigin;
+static Direction curDestination;
 
-static volatile int nw = 0;
-static volatile int ne = 0;
-static volatile int ns = 0;
-static volatile int wn = 0;
-static volatile int ws = 0;
-static volatile int we = 0;
-static volatile int en = 0;
-static volatile int es = 0;
-static volatile int ew = 0;
-static volatile int sw = 0;
-static volatile int se = 0;
-static volatile int sn = 0;
+bool checkIntersection(Direction origin, Direction destination);
+void decrement(Direction origin, Direction destination);
 
-/*
+/* 
  * This simple default synchronization mechanism allows only vehicle at a time
  * into the intersection.   The intersectionSem is used as a a lock.
  * We use a semaphore rather than a lock so that this code will work even
  * before locks are implemented.
  */
 
-/*
+/* 
  * Replace this default synchronization mechanism with your own (better) mechanism
  * needed for your solution.   Your mechanism may use any of the available synchronzation
- * primitives, e.g., semaphores, locks, condition variables.   You are also free to
+ * primitives, e.g., semaphores, locks, condition variables.   You are also free to 
  * declare other global variables if your solution requires them.
  */
 
 /*
  * replace this with declarations of any synchronization and other variables you need here
  */
-static struct cv *maCv;
-static struct lock *maLock;
-
-/*
+static struct lock* intersectionLock;
+static struct cv* intersectionCV;
+static struct cv* firstWaitingCV;
+/* 
  * The simulation driver will call this function once before starting
  * the simulation
  *
  * You can use it to initialize synchronization and other variables.
- *
+ * 
  */
 void
 intersection_sync_init(void)
 {
-  maCv = cv_create("maCv");
-  if (maCv == NULL) {
-    panic("could not create cv");
+  /* replace this default implementation with your own implementation */
+
+  intersectionLock = lock_create("intersectionLock");
+  intersectionCV = cv_create("intersectionCV");
+  firstWaitingCV = cv_create("firstWaitingCV");
+
+  if (intersectionLock == NULL) {
+    panic("could not create intersection semaphore");
+  } else if (intersectionCV == NULL) {
+    panic("could not create intersection cv");
+  } else if (firstWaitingCV == NULL) {
+    panic("could not create firstWaiting cv");
   }
-  maLock = lock_create("maLock");
-  if (maLock == NULL) {
-    panic("could not create lock");
-  }
+
   return;
 }
 
-/*
+/* 
  * The simulation driver will call this function once after
  * the simulation has finished
  *
@@ -70,10 +80,97 @@ intersection_sync_init(void)
 void
 intersection_sync_cleanup(void)
 {
-  KASSERT(maCv != NULL);
-  cv_destroy(maCv);
-  KASSERT(maLock != NULL);
-  lock_destroy(maLock);
+  /* replace this default implementation with your own implementation */
+  KASSERT(intersectionCV != NULL);
+  KASSERT(firstWaitingCV != NULL);
+  KASSERT(intersectionLock != NULL);
+
+  lock_destroy(intersectionLock);
+  cv_destroy(intersectionCV);
+  cv_destroy(firstWaitingCV);
+}
+
+// Check if it will collide with other car that is at the intersection
+bool checkIntersection(Direction origin, Direction destination) {
+  if (origin == north) {  // North
+    if (destination == south
+      && !ew && !we && !sw && !wn && !ws && !es) {  // North South
+      ++ns;
+    } else if (destination == east
+      && !sn && !sw && !se && !we && !wn && !es && !ew) {  // North East
+      ++ne;
+    } else if (destination == west && !sw && !ew) {   // North West
+      ++nw;
+    } else {
+      return false;
+    }
+  } else if (origin == south) {  // South
+    if (destination == north
+      && !ne && !we && !wn && !ew && !es && !en) {  // South North
+      ++sn;
+    } else if (destination == east && !ne && !we) {    // South East
+      ++se;
+    } else if (destination == west
+      && !ns && !nw && !ne && !we && !wn && !ew && !es) {  // South West
+      ++sw;
+    } else {
+      return false;
+    }
+  } else if (origin == east) {   // East
+    if (destination == west
+      && !sn && !sw && !ns && !nw && !ne && !wn) {  // East West
+      ++ew;
+    } else if (destination == north && !sn && !wn) {      // East North
+      ++en;
+    } else if (destination == south
+      && !sn && !sw && !ns && !ne && !we && !ws && !wn) { // East South
+      ++es;
+    } else {
+      return false;
+    }
+  } else if (origin == west) { // West
+    if (destination == east
+      && !sn && !se && !sw && !ns && !ne && !es) { // West East
+      ++we;
+    } else if (destination == north
+      && !sn && !sw && !ns && !ne && !ew && !en && !es) {  // West North
+      ++wn;
+    } else if (destination == south && !ns && !es) {  // West South
+      ++ws;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Decrement the number of cars in the given direction
+void decrement(Direction origin, Direction destination) {
+  if (origin == north && destination == south) {
+    --ns;
+  } else if (origin == north && destination == east) {
+    --ne;
+  } else if (origin == north && destination == west) {
+    --nw;
+  } else if (origin == south && destination == north) {
+    --sn;
+  } else if (origin == south && destination == east) {
+    --se;
+  } else if (origin == south && destination == west) {
+    --sw;
+  } else if (origin == east && destination == west) {
+    --ew;
+  } else if (origin == east && destination == north) {
+    --en;
+  } else if (origin == east && destination == south) {
+    --es;
+  } else if (origin == west && destination == east) {
+    --we;
+  } else if (origin == west && destination == north) {
+    --wn;
+  } else if (origin == west && destination == south) {
+    --ws;
+  }
 }
 
 
@@ -89,128 +186,33 @@ intersection_sync_cleanup(void)
  *
  * return value: none
  */
-bool allowed(Direction origin, Direction destination)
-{
-    if(origin == 0)//N
-    {
-        if(destination == 3)//NW
-        {
-          if(ew != 0 || sw != 0)
-              return false;
-          else
-              nw++;
-        }
-        else if(destination == 2)//NS
-        {
-          if(wn != 0 || ws != 0 || we != 0 || es != 0 || ew != 0 || sw != 0)
-              return false;
-          else
-              ns++;
-        }
-        else//NE
-        {
-          if(wn != 0 || we != 0 || es != 0 || ew != 0 || sn != 0 || sw != 0 || se != 0)
-              return false;
-          else
-              ne++;
-        }
-    }
-    else if(origin == 1)//E
-    {
-      if(destination == 0)//EN
-      {
-        if(wn != 0 || sn != 0)
-            return false;
-        else
-            en++;
-      }
-      else if(destination == 2)//ES
-      {
-        if(ne != 0 || ns != 0 || wn != 0 || ws != 0 || we != 0 || sn != 0 || sw != 0)
-            return false;
-        else
-            es++;
-      }
-      else//3 EW
-      {
-        if(ne != 0 || nw!=0 || ns != 0 || wn != 0 || sn != 0 || sw != 0)
-            return false;
-        else
-            ew++;
-      }
-    }
-
-    else if(origin == 2)//S
-    {
-        if(destination == 0)//SN
-        {
-          if(ne != 0 || wn != 0 || we != 0 || en != 0 || es != 0 || ew != 0)
-              return false;
-          else
-              sn++;
-        }
-        else if(destination == 1)//SE
-        {
-          if(ne != 0 || we != 0)
-              return false;
-          else
-              se++;
-        }
-        else//3 SW
-        {
-          if(nw != 0 || ne != 0 || ns != 0 || wn != 0 || we != 0 || es != 0 || ew != 0)
-              return false;
-          else
-              sw++;
-        }
-    }
-    else //W
-    {
-        if(destination == 0)//WN
-        {
-          if(ne != 0 || en != 0 || es != 0 || ew != 0 || sn != 0 || sw != 0)
-              return false;
-          else
-              wn++;
-        }
-        else if(destination == 1)//WE
-        {
-          if(nw != 0 || ne != 0 || ns != 0 || es != 0 || sn != 0 || sw != 0)
-              return false;
-          else
-              we++;
-        }
-        else//2 WS
-        {
-          if(ns != 0 || es != 0)
-              return false;
-          else
-              ws++;
-        }
-    }
-    return true;
-}
 
 void
 intersection_before_entry(Direction origin, Direction destination)
 {
   /* replace this default implementation with your own implementation */
-KASSERT(maLock != NULL);
-lock_acquire(maLock);
-  while(allowed(origin, destination) == false)
-  {
-      cv_wait(maCv, maLock);
+  KASSERT(intersectionCV != NULL);
+  KASSERT(firstWaitingCV != NULL);
+  KASSERT(intersectionLock != NULL);
+
+  lock_acquire(intersectionLock);
+  while ((requestStop && origin != curOrigin && destination != curDestination)
+      || !checkIntersection(origin, destination)) {
+    if (requestStop == false) {
+      requestStop = true;
+      cv_wait(firstWaitingCV, intersectionLock);
+    } else {
+      cv_wait(intersectionCV, intersectionLock);
+    }
   }
-  cv_signal(maCv, maLock);
-
-
-  lock_release(maLock);
+  curOrigin = origin;
+  curDestination = destination;
+  lock_release(intersectionLock);
 }
-
 
 /*
  * The simulation driver will call this function each time a vehicle
- * leaves the intersection.
+ * leaves the intersction.
  *
  * parameters:
  *    * origin: the Direction from which the vehicle arrived
@@ -222,50 +224,14 @@ lock_acquire(maLock);
 void
 intersection_after_exit(Direction origin, Direction destination)
 {
-  KASSERT(maLock != NULL);
-  lock_acquire(maLock);
-  /* replace this default implementation with your own implementation */
-  if(origin == 0)//N
-  {
-      if(destination == 1)//NE
-          ne--;
-      else if(destination == 2)//NS
-          ns--;
-      else//3 NW
-          nw--;
-  cv_signal(maCv, maLock);
-  }
-  else if(origin == 1)//E
-  {
-      if(destination == 0)//EN
-          en--;
-      else if(destination == 2)//ES
-          es--;
-      else//3 EW
-          ew--;
-  cv_signal(maCv, maLock);
-  }
-  else if(origin == 2)//S
-  {
-      if(destination == 0)//SN
-          sn--;
-      else if(destination == 1)//SE
-          se--;
-      else//3 SW
-          sw--;
-  cv_signal(maCv, maLock);
-  }
-  else//(origin == 3)W
-  {
-      if(destination == 0)//WN
-          wn--;
-      else if(destination == 1)//WE
-          we--;
-      else//2 WS
-          ws--;
+  KASSERT(intersectionCV != NULL);
+  KASSERT(firstWaitingCV != NULL);
+  KASSERT(intersectionLock != NULL);
 
-  cv_signal(maCv, maLock);
-  }
-
-  lock_release(maLock);
+  lock_acquire(intersectionLock);
+  decrement(origin, destination);
+  requestStop = false;
+  cv_signal(firstWaitingCV, intersectionLock);
+  cv_broadcast(intersectionCV, intersectionLock);
+  lock_release(intersectionLock);
 }
