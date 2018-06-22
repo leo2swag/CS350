50c879367b06868d293c607d5e2d1bf40b6f536f
leo2swag <leoliush1137@gmail.com>
2018-06-22 16:52:17 -0400

    attemp a2b 2

--- os161-1.99/kern/arch/mips/syscall/syscall.c
+++ os161-1.99/kern/arch/mips/syscall/syscall.c
@@ -135,7 +135,7 @@ syscall(struct trapframe *tf)
     case SYS_fork:
     	err = sys_fork(tf, (pid_t *)&retval);
 		break;
-	case SYS_execv;
+	case SYS_execv:
 		err = sys_execv((char *)tf->tf_a0, (char **)tf->tf_a1);
 #endif
 	    /* Add stuff here */
