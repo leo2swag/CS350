fd8d5772eb3521632cc8772115364e7fe71e0a7e
leo2swag <leoliush1137@gmail.com>
2018-07-11 21:17:44 -0400

    update

--- os161-1.99/kern/arch/mips/vm/dumbvm.c
+++ os161-1.99/kern/arch/mips/vm/dumbvm.c
@@ -337,9 +337,9 @@ void
 as_destroy(struct addrspace *as)
 {
 #if OPT_A3
-	kfree((void *)PADDR_TO_KVADDR(as->as_pbase1));
-	kfree((void *)PADDR_TO_KVADDR(as->as_pbase2));
-	kfree((void *)PADDR_TO_KVADDR(as->as_stackpbase));
+	kfree(PADDR_TO_KVADDR(as->as_pbase1));
+	kfree(PADDR_TO_KVADDR(as->as_pbase2));
+	kfree(PADDR_TO_KVADDR(as->as_stackpbase));
 #else
 	kfree(as);
 #endif
