fba5c138b48e01ff27e2a74166e31a877a1ae673
leo2swag <leoliush1137@gmail.com>
2018-07-11 20:14:18 -0400

    a3 attemp 4

--- os161-1.99/kern/arch/mips/locore/trap.c
+++ os161-1.99/kern/arch/mips/locore/trap.c
@@ -107,7 +107,11 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 		sig = SIGFPE;
 		break;
 	}
-
+	#if OPT_A3
+		(void) epc;
+		(void) vaddr;
+		sys__exit(sig);
+	#else
 	/*
 	 * You will probably want to change this.
 	 */
@@ -115,6 +119,7 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
 		code, sig, trapcodenames[code], epc, vaddr);
 	panic("I don't know how to handle this\n");
+	#endif
 }
 
 /*
